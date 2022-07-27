214f506f1f67f58a2077689c821df22be7e2a916
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-07-28 00:13:00 +0800

    Build an animation controller

--- Assets/Scripts/Interaction/Trigger.cs
+++ Assets/Scripts/Interaction/Trigger.cs
@@ -4,17 +4,22 @@ using NaughtyAttributes;
 
 public class Trigger : MonoBehaviour {
 	[Tag] public string tagMask;
+	public bool oneTime = false;
 	public UnityEvent<Collider> onEnter;
 	public UnityEvent<Collider> onExit;
 
+	bool Validate(Collider other) => string.IsNullOrEmpty(tagMask) || other.tag == tagMask;
+
 	public void OnTriggerEnter(Collider other) {
-		if(other.tag != tagMask)
+		if(!Validate(other))
 			return;
 		onEnter.Invoke(other);
+		if(oneTime)
+			Destroy(this);
 	}
 
 	public void OnTriggerExit(Collider other) {
-		if(other.tag != tagMask)
+		if(!Validate(other))
 			return;
 		onExit.Invoke(other);
 	}
