7ed6695cbd1a01fa0c02e238c4b9e648a7bab662
Nianyi Wang <Wangnianyi2001@outlook.com>
2023-03-07 18:47:15 +0800

    Fix camera selector raycasting bug

--- Assets/Modules/Interaction/CameraInteractor.cs
+++ Assets/Modules/Interaction/CameraInteractor.cs
@@ -3,86 +3,83 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Game
-{
-    public class CameraInteractor : MonoBehaviour
-    {
-        #region Inspector members
-        public new Camera camera;
-        public float maxDistance = 10;
-        #endregion
+namespace Game {
+	public class CameraInteractor : MonoBehaviour {
+		#region Inspector members
+		public new Camera camera;
+		public float maxDistance = 10;
+		public bool raycastAll = false;
+		#endregion
 
-        #region Core members
-        bool activity = false;
-        [NonSerialized] public List<InteractableTarget> lastFocused;
-        Vector3 lastDirection;
-        #endregion
+		#region Core members
+		bool activity = false;
+		[NonSerialized] public List<InteractableTarget> lastFocused;
+		#endregion
 
-        #region Public interfaces
-        public bool Activity
-        {
-            get => activity;
-            set
-            {
-                if (activity == value)
-                    return;
-                activity = value;
-                if (activity)
-                {
-                    foreach (InteractableTarget target in lastFocused)
-                    {
-                        if (!target.focused)
-                            target.OnFocus(this);
-                        if (!target.activated)
-                            target.OnActivate(this);
-                    }
-                }
-                else
-                {
-                    foreach (InteractableTarget target in lastFocused)
-                    {
-                        if (target.activated)
-                            target.OnDeactivate(this);
-                        if (target.focused)
-                            target.OnBlur(this);
-                    }
-                }
-            }
-        }
-        #endregion
+		#region Public interfaces
+		public bool Activity {
+			get => activity;
+			set {
+				if(activity == value)
+					return;
+				activity = value;
+				if(activity) {
+					foreach(InteractableTarget target in lastFocused) {
+						if(!target.focused)
+							target.OnFocus(this);
+						if(!target.activated)
+							target.OnActivate(this);
+					}
+				}
+				else {
+					foreach(InteractableTarget target in lastFocused) {
+						if(target.activated)
+							target.OnDeactivate(this);
+						if(target.focused)
+							target.OnBlur(this);
+					}
+				}
+			}
+		}
+		#endregion
 
-        public void Start()
-        {
-            if (camera == null)
-                camera = GetComponent<Camera>();
-            lastFocused = new List<InteractableTarget>();
-        }
+		public void Start() {
+			if(camera == null)
+				camera = GetComponent<Camera>();
+			lastFocused = new List<InteractableTarget>();
+		}
 
-        public void FixedUpdate()
-        {
-            if (camera == null)
-                return;
-            Ray ray = camera.ScreenPointToRay(new Vector2(camera.pixelWidth, camera.pixelHeight) / 2);
-            RaycastHit[] hits = Physics.RaycastAll(ray, maxDistance);
-            var currentFocused = hits
-                .Select((RaycastHit hit) => hit.collider.GetComponent<InteractableTarget>())
-                .Where((InteractableTarget usable) => usable != null)
-                .ToList();
-            foreach (InteractableTarget target in currentFocused)
-            {
-                if (!lastFocused.Contains(target))
-                    target.OnFocus(this);
-            }
-            foreach (InteractableTarget target in lastFocused)
-            {
-                if (!currentFocused.Contains(target))
-                {
-                    if (target.activated)
-                        target.OnDeactivate(this);
-                    target.OnBlur(this);
-                }
-            }
-            lastFocused = currentFocused;
-        }
-    }
+		public void FixedUpdate() {
+			if(camera == null)
+				return;
+			Ray ray = camera.ScreenPointToRay(new Vector2(camera.pixelWidth, camera.pixelHeight) / 2);
+			var hits = new List<RaycastHit>();
+			if(raycastAll) {
+				var hitsArr = Physics.RaycastAll(ray, maxDistance);
+				hits.AddRange(hitsArr);
+			}
+			else {
+				RaycastHit hit;
+				Physics.Raycast(ray, out hit, maxDistance);
+				if(hit.collider)
+					hits.Add(hit);
+			}
+			var currentFocused = hits
+				.Select((RaycastHit hit) => hit.collider?.GetComponent<InteractableTarget>())
+				.Where((InteractableTarget usable) => usable != null)
+				.ToList();
+			foreach(InteractableTarget target in currentFocused) {
+				if(!lastFocused.Contains(target))
+					target.OnFocus(this);
+			}
+			foreach(InteractableTarget target in lastFocused) {
+				if(!currentFocused.Contains(target)) {
+					if(target.activated)
+						target.OnDeactivate(this);
+					target.OnBlur(this);
+				}
+			}
+			lastFocused = currentFocused;
+		}
+	}
 }
