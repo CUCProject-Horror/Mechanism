0763e45e20dd68bed1981c14ab6dfaf50229d8f6
mzt <maiijortom@qq.com>
2023-03-19 17:50:47 +0800

    Include Extended Packs

---
+++ Assets/ExtendedPac/Jitter/Examples/Scripts/CustomUsageExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using VirtualEscapes.Common.Jitter;
+
+namespace VirtualEscapes.Common.JitterExample
+{
+    /// <summary>
+    /// Jitter instantiator.
+    /// Demonstrates how to instantiate Jitter from a script at runtime, rather than have it attached to a gameobject/prefab.
+    /// </summary>
+    public class CustomUsageExample : MonoBehaviour
+    {
+        private JitterCustom jitter => GetComponent<JitterCustom>();
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            float value = jitter.jitterValue;
+
+            transform.localPosition = new Vector3(0, value, 0);
+            transform.localScale = new Vector3(1, 1, value * 4);
+        }
+
+    }
+}
---
+++ Assets/ExtendedPac/Jitter/Examples/Scripts/JitterInstantiator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+using VirtualEscapes.Common.Jitter;
+
+namespace VirtualEscapes.Common.JitterExample
+{
+    /// <summary>
+    /// Jitter instantiator.
+    /// Demonstrates how to instantiate Jitter from a script at runtime, rather than have it attached to a gameobject/prefab.
+    /// </summary>
+    public class JitterInstantiator : MonoBehaviour
+    {
+        private const float SHAKE_DURATION = 2.0f;
+
+        private JitterPosition mJitterPosition;
+        private JitterRotation mJitterRotation;
+        private JitterScale mJitterScale;
+
+        private float mfShakeTimerStart;
+        private bool mbIsPaused;
+
+        void Start()
+        {
+            mJitterPosition = gameObject.AddComponent<JitterPosition>();
+            mJitterPosition.Initialise(1, -1, 1, 0.15f, AxisMasks.X | AxisMasks.Y | AxisMasks.Z, false);
+
+            mJitterRotation = gameObject.AddComponent<JitterRotation>();
+            mJitterRotation.Initialise(1, -45, 45, 0.25f, AxisMasks.X | AxisMasks.Y | AxisMasks.Z, false);
+
+            mJitterScale = gameObject.AddComponent<JitterScale>();
+            //if initialise is not called then default values will be used
+
+            //but jitter properties can also be set/get individually
+            mJitterScale.seed = 12345;
+            mJitterScale.RandomizeSeed();
+            mJitterScale.frequency = 0.25f;
+            mJitterScale.amplitudeRange.start = 0;
+            mJitterScale.amplitudeRange.end = 0.5f;
+            mJitterScale.axisMask = AxisMasks.X | AxisMasks.Y | AxisMasks.Z;
+            mJitterScale.axisLocked = true;
+
+            mfShakeTimerStart = Time.time - SHAKE_DURATION;
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //make it shake
+                mfShakeTimerStart = Time.time;
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                //toggle pausing of the Jitter
+                mbIsPaused = !mbIsPaused;
+                Time.timeScale = mbIsPaused ? 0 : 1;
+            }
+
+            setMagnitudes(Mathf.Clamp01(1.0f - (Time.time - mfShakeTimerStart) / SHAKE_DURATION));
+        }
+
+        private void setMagnitudes(float magnitude)
+        {
+            mJitterPosition.magnitude = mJitterRotation.magnitude = mJitterScale.magnitude = magnitude;
+        }
+    }
+}
